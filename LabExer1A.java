13113cc9e45b10bb092df1a12b72e992dc9de77d
yzabelle2000 <58801449+yzabelle2000@users.noreply.github.com>
2019-12-12 14:47:07 +0800

    Add files via upload

---
+++ LabExer1A.java
@@ -0,0 +1,15 @@
+public class LabExer1A
+{
+   public static void main (String [] args) {
+   int faveNumber = 16;
+   String faveCartChar = "Finn and Jake";
+   char Middle = 'J';
+   char[]NicknameArray = {'M','A','R','Y','Z'};
+   
+   System.out.println(faveNumber + " is my favorite number");
+   System.out.println("I love " + faveCartChar  + " is my favorite cartoon character");
+   System.out.println( Middle  + " is my middle initial");
+   System.out.print("You can call me " + NicknameArray[0] + NicknameArray[1] + NicknameArray [2] + NicknameArray [3] + NicknameArray[4]);
+   }
+}
+  
